5cbdd10a420084e1152787d1dccaa95a730a3a3b
Rocky Singh <rockygsingh@icloud.com>
2019-03-19 16:26:11 -0400

    routes so you can search

---
+++ routes/books.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+router.get('/bookResults', (req, res) => {
+    res.render('bookResults');
+});
+
+
+router.post('/bookResults', (req, res) => {
+
+    const searchText = req.query.searchForBook;
+        // making call to google books api with a search word
+        const URL = 'https://www.googleapis.com/books/v1/volumes?q=' + searchText + '&maxResults=40&orderBy=relevance';
+        request(URL, (err, response, body) => {
+              if (!err && response.statusCode == 200) {
+                  let apiResultsdata = JSON.parse(body);
+                  res.render('bookResults', { apiResultsdata: apiResultsdata });
+              } else {
+                  console.log(err);
+              }
+        });
+});
+
+
+module.exports = router;
