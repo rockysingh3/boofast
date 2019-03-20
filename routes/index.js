efe4c63c9d92ddd2864c924cdf66c59051c7f738
Rocky Singh <rockygsingh@icloud.com>
2019-03-20 17:02:50 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-
 // Welcome page
-router.get('/', (req,res) => res.render('welcome'));
+router.get('/', (req,res) => {
+    res.render('welcome');
+})
 
 // Dashboard
 router.get('/dashboard', (req, res) => {
--- routes/user.js
+++ routes/user.js
@@ -84,7 +84,7 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
-        successRedirect: '/dashboard',
+        successRedirect: '/',
         failureRedirect: '/user/login',
         failureFlash: true
     })(req, res, next);
