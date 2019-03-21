bdb05949afb1e0a47c33c70948087115e3764ca0
Rocky Singh <rockygsingh@icloud.com>
2019-03-21 14:12:22 -0400

    moved single book route into index.js

--- routes/books.js
+++ routes/books.js
@@ -7,24 +7,14 @@ const { ensureAutheticated } = require('../config/auth');
 //     res.render('bookResults');
 // });
 
-const getJsonData = async (url) => {
-    try {
-        return await axios.get(url)
-    }catch (err) {
-        console.log(err);
-    }
-}
 
 
+// renders a list of books user searched for
 router.post('/bookResults', ensureAutheticated, (req, res) => {
 
 
         const searchText = req.query.searchForBook;
         const URL =   `https://www.googleapis.com/books/v1/volumes?q=${searchText}&maxResults=40&orderBy=relevance`;
-        // const data = getJsonData(URL)
-        //                     .then((response) => JSON.parse(response))
-        //                     .then((json) => json);
-        //                     .catch(err => console.log(err));
 
         
         request(URL, (err, response, body) => {
@@ -36,6 +26,30 @@ router.post('/bookResults', ensureAutheticated, (req, res) => {
             }
       });
 });
+
+
+
+// reders a single books user clicked on
+router.get('/bookdetails/:id', (req, res) => {
+    //console.log(`req is : ${req.body.ID}`);
+    let id = req.params.id;
+      //making a call to google books api with a ID of one book 
+      const URL = 'https://www.googleapis.com/books/v1/volumes/' + id;
+      request(URL, (err, response, body) => {
+            if (!err && response.statusCode == 200) {
+                let bookdata = JSON.parse(body);
+                res.render('singlebookdetails', { bookdata: bookdata });
+            } else {
+                console.log(err);
+            }
+      });
+  });
+
+
+  router.get('/summary', (req, res) => {
+      let isAuth = req.isAuthenticated();
+      res.render('summaries', { isAuth:isAuth });
+  });
 
 
 module.exports = router;
